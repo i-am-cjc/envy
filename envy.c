6464cff685191999f53458e7e2ce68125c74abc5
Carl Clegg <carl@cjc.im>
2017-04-06 11:53:16 +0100

    Raw mode chapter done

--- envy.c
+++ envy.c
@@ -1,7 +1,76 @@
 #include <unistd.h>
+#include <termios.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <errno.h>
+
+struct termios origTermios;
+
+void die(const char *s) {
+    perror(s);
+    exit(1);
+}
+
+void disableRawMode() {
+    // set the original flags back to the terminal
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &origTermios) == -1)
+        die("tcsetattr");
+}
+
+void enableRawMode() {
+
+    // take copies of the current terminal setup
+    if (tcgetattr(STDIN_FILENO, &origTermios) == -1)
+        die("tcsetattr");
+
+    struct termios raw = origTermios;
+
+    // reset when we exit
+    atexit(disableRawMode);
+
+    // bit wise ANDing lflag against A NOTted ECHO flag set 
+    // this allows us to unset the ECHO flag set whilst retaining the rest
+    // of the lflags (local flags from termios)
+    // ICANON flag allows us to turn off canonical mode, ie we will be reading
+    // in input byte by byte rather than a line at a time
+    // ISIG stops C-z and C-c
+    // IEXTEN stops C-v and C-o
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+
+    // turn off the IXON flag, which stops C-s and C-q from doing anything
+    // ICRNL stops terminal translating things for us like C-m 
+    raw.c_iflag &= ~(BRKINT | INPCK | ISTRIP | ICRNL | IXON);
+
+    // Turn off all output processing
+    raw.c_oflag &= ~(OPOST);
+
+    // Set the character size to 8 just incase
+    raw.c_cflag |= (CS8);
+
+    // Set some control characters to allow read to timeout and not block
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+        die("tcsetattr");
+}
 
 int main() {
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1);
+    enableRawMode();
+
+    while (1) {
+        char c = '\0';
+        if (read(STDIN_FILENO, &c, 1) == -1 
+                && errno != EAGAIN) die("read");
+
+        if (isprint(c)) {
+            printf("%d ('%c')\r\n", c, c);
+        } else {
+            printf("%d\r\n", c);
+        }
+        if (c == 'q') break;
+    }
+
     return 0;
 }
