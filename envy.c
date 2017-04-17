bf918b52748a74d6cae0f471611d7e9d7146b383
cjc <cjc@localhost.localdomain>
2017-04-17 11:19:13 +0100

    Full searching, incremental search, and cycling search hits

--- envy.c
+++ envy.c
@@ -56,7 +56,7 @@ struct editorConfig {
 struct editorConfig E;
 
 /*** prototypes ***/
-char *ePrompt(char *prompt);
+char *ePrompt(char *prompt, void (*callback)(char *, int));
 
 // APPEND BUFFER
 struct abuf {
@@ -295,6 +295,20 @@ int eRowCxToRx(erow *row, int cx) {
     return rx;
 }
 
+int erowRxToCx(erow *row, int rx) {
+    int cur_rx = 0;
+    int cx;
+    for (cx = 0; cx < row->size; cx++) {
+        if (row->chars[cx] == '\t')
+            cur_rx += (ENVY_TAB_STOP - 1) - (cur_rx % ENVY_TAB_STOP);
+        cur_rx++;
+
+        if (cur_rx > rx) return cx;
+    }
+
+    return cx;
+}
+
 void eUpdateRow(erow *row) {
     int tabs = 0;
     int i;
@@ -462,7 +476,7 @@ void eOpen(char *filename) {
 
 void eSave() {
     if (E.filename == NULL) 
-        E.filename = ePrompt("Filename: %s");
+        E.filename = ePrompt("Filename: %s", NULL);
     // If the prompt was aborted we are NULL again
     if (E.filename == NULL) {
         eSetStatusMessage("Aborted");
@@ -491,6 +505,63 @@ void eSave() {
     eSetStatusMessage("Error writing to disk: %s", strerror(errno));
 }
 
+/*** search and find ***/
+void eFindCallback(char *query, int key) {
+	static int last_match = -1;
+	static int direction = 1;
+
+    if (key == '\r' || key == '\x1b') {
+		last_match = -1;
+		direction = 1;
+        return;
+	} else if (key == DOWN) {
+		direction = 1;
+	} else if (key == UP) {
+		direction = -1;
+	} else {
+		last_match = -1;
+		direction = 1;
+	}
+
+	if (last_match == -1) direction = 1;
+	int current = last_match;
+
+    int i;
+    for (i = 0; i < E.numrows; i++) {
+		current += direction;
+		if (current == -1) current = E.numrows - 1;
+		else if (current == E.numrows) current = 0;
+
+        erow *row = &E.row[current];
+        char *match = strstr(row->render, query);
+        if (match) {
+			last_match = current;
+            E.cy = current;
+            E.cx = erowRxToCx(row, match - row->render);
+            E.rowoff = E.numrows;
+            break;
+        }
+    }
+}
+
+void eFind() {
+    int saved_cx = E.cx;
+    int saved_cy = E.cy;
+    int saved_coloff = E.coloff;
+	int saved_rowoff = E.rowoff;
+
+    char *query = ePrompt("Find: %s", eFindCallback);
+
+    if (query) {
+		free(query);
+	} else {
+		E.cx = saved_cx;
+		E.cy = saved_cy;
+		E.coloff = saved_coloff;
+		E.rowoff = saved_rowoff;
+	}
+}
+
 int getWindowSize(int *rows, int *cols) {
     struct winsize ws;
 
@@ -504,7 +575,7 @@ int getWindowSize(int *rows, int *cols) {
 }
 
 /** input **/
-char *ePrompt(char *prompt) {
+char *ePrompt(char *prompt, void (*callback)(char *, int)) {
     size_t bufsize = 128;
     char *buf = malloc(bufsize);
 
@@ -520,11 +591,13 @@ char *ePrompt(char *prompt) {
             if (buflen != 0) buf[--buflen] = '\0';
         } else if (c == '\x1b') {
             eSetStatusMessage("");
+            if (callback) callback(buf, c);
             free(buf);
             return NULL;
         } else if (c == '\r') {
             if (buflen != 0) {
                 eSetStatusMessage("");
+                if (callback) callback(buf, c);
                 return buf;
             }
         } else if (!iscntrl(c) && c < 128) {
@@ -535,6 +608,8 @@ char *ePrompt(char *prompt) {
             buf[buflen++] = c;
             buf[buflen] = '\0';
         }
+
+        if (callback) callback(buf, c);
     }
 }
 
@@ -587,6 +662,11 @@ void eProcessKeypress() {
         case CTRL_KEY('d'):
             eDelRow(E.cy);
             break;
+
+        case CTRL_KEY('f'):
+            eFind();
+            break;
+
         case CTRL_KEY('q'):
             if (E.dirty && quit_times > 0) {
                 eSetStatusMessage("File changed. "
